69a6767168f1643ce2f0104e75cf3f3b948b71d9
Lucille Gris <lucille.gris@gmail.com>
2024-11-12 17:02:17 +0100

    exercice 3 non terminé (sauvegarde) - rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |i|
+  Article.new({ title: "title#{i}", content: "content#{i}" })
+end
